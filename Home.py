93094fb11ed48cd7dcc453974ab7dfed84b9b350
daehyeong2 <baconbacon1231@gmail.com>
2024-04-07 13:10:35 +0900

    Magic (7.1)

--- Home.py
+++ Home.py
@@ -1,12 +1,13 @@
 import streamlit as st
 
-st.title("Hello world!")
-
-st.subheader("Welcome to Streamlit!!!")
-
-st.markdown(
-    """
-##### 안녕하세요
-- 테스트
-"""
+option = st.selectbox(
+    ":violet[당신이 좋아하는 자동차는? :car:]",
+    ("람보르기니", "부가티", "페라리"),
+    index=1,
+    placeholder="좋아하는 자동차를 선택하세요.",
 )
+
+if option:
+    st.write(f"당신이 좋아하는 자동차는 {option}입니다.")
+else:
+    st.write("좋아하는 자동차를 선택해주세요.")
