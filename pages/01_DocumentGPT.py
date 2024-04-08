8fbe661b947afeaec5c72810b53eeadd228f7b96
daehyeong2 <baconbacon1231@gmail.com>
2024-04-08 09:25:56 +0900

    파일 업로드 (7.5~7.6)

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,29 +1,50 @@
 import time
 import streamlit as st
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.document_loaders import UnstructuredFileLoader
+
+st.set_page_config(page_title="DocumentGPT", page_icon="📜")
 
 st.title("DocumentGPT")
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+st.markdown(
+    """
+환영합니다!
+
+이 챗봇을 사용해서 당신의 문서에 대해 물어보세요!
+"""
+)
 
 
-def send_message(message, role, save=True):
-    with st.chat_message(role):
-        st.write(message)
-    if save:
-        st.session_state["messages"].append({"message": message, "role": role})
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n", chunk_size=600, chunk_overlap=100
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+
+    retriever = vectorstore.as_retriever()
+
+    return retriever
 
 
-for message in st.session_state["messages"]:
-    send_message(message["message"], message["role"], save=False)
+file = st.file_uploader(
+    ".txt .pdf .docx 파일을 업로드하세요.", type=["pdf", "txt", "docx"]
+)
 
-
-message = st.chat_input("AI에게 메시지를 보내세요.")
-
-if message:
-    send_message(message, "human")
-    time.sleep(2)
-    send_message(f"You said: {message}", "ai")
-
-    with st.sidebar:
-        st.write(st.session_state)
+if file:
+    retriever = embed_file(file)
+    doc = retriever.invoke("재귀함수는 무엇인가요?")
+    st.write(doc)
