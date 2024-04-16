f7428d1640cf58ca6712fa8fdd1737e446e0cc85
daehyeong2 <baconbacon1231@gmail.com>
2024-04-16 20:58:32 +0900

    AsyncChromiumLoader (10.0~10.1)

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,3 +1,31 @@
+from langchain.document_loaders import AsyncChromiumLoader
+from langchain.document_transformers import Html2TextTransformer
 import streamlit as st
 
-st.title('SiteGPT')
+
+st.set_page_config(page_title="SiteGPT", page_icon="🖥️")
+
+st.title("SiteGPT")
+
+
+st.markdown(
+    """
+            웹 사이트의 내용에 대해 궁금한 것을 질문해 보세요.
+
+            사이드 바에서 웹 사이트의 URL을 입력해서 시작하세요.
+"""
+)
+
+with st.sidebar:
+    url = st.text_input(
+        "URL을 입력해 주세요.",
+        placeholder="https://example.com",
+    )
+
+html2text_transformer = Html2TextTransformer()
+
+if url:
+    loader = AsyncChromiumLoader([url])
+    docs = loader.load()
+    transformed = html2text_transformer.transform_documents(docs)
+    st.write(transformed)
